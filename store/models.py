4dba1a25057c59f1643043cc616c997dda9c06ca
BasharNatour <bashar.nat1997@gmail.com>
2022-04-18 02:02:00 +0200

    add

--- store/models.py
+++ store/models.py
@@ -15,6 +15,11 @@ class Promotion(models.Model):
 
 
 
+class Collection(models.Model):
+    title = models.CharField(max_length=255)
+    featured_product = models.ForeignKey('Product' , on_delete=models.SET_NULL , null= True , related_name='+')
+
+
 class Product(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
@@ -22,7 +27,9 @@ class Product(models.Model):
     inventory = models.IntegerField()
     last_update = models.DateField(auto_now=True)
     promotions   = models.ManyToManyField(Promotion)
+    collection  = models.ForeignKey(Collection, on_delete=models.PROTECT)
     tags =  GenericRelation(TaggedItem)
+
 
 
 
@@ -89,18 +96,16 @@ class OrderItem(models.Model):
 
 
 
-class Collection(models.Model):
-    title = models.CharField(max_length=255)
-    featured_product = models.ForeignKey('Product' , on_delete=models.SET_NULL , null= True , related_name='+')
 
 
 
-class Product(models.Model):
-    name= models.CharField(max_length=255)
-    description = models.TextField()
-    price = models.DecimalField(max_digits=6 , decimal_places=2)
-    inventory = models.IntegerField()
-    last_update = models.DateTimeField(auto_now=True)
-    collection  = models.ForeignKey(Collection, on_delete=models.PROTECT)
+
+# class Product(models.Model):
+#     name= models.CharField(max_length=255)
+#     description = models.TextField()
+#     price = models.DecimalField(max_digits=6 , decimal_places=2)
+#     inventory = models.IntegerField()
+#     last_update = models.DateTimeField(auto_now=True)
+#     collection  = models.ForeignKey(Collection, on_delete=models.PROTECT)
 
 
--- tags/models.py
+++ tags/models.py
@@ -1,8 +1,7 @@
 from operator import mod
 from django.db import models
-from store.models import Product
 from django.contrib.contenttypes.models import ContentType 
-from django.contrib.contenttypes.models import GenericForeignKey 
+from django.contrib.contenttypes.fields import GenericForeignKey 
 # Create your models here.
 class Tag(models.Model):
     label = models.CharField(max_length=255)
@@ -13,4 +12,4 @@ class TaggedItem(models.Model):
     tag = models.ForeignKey(Tag , on_delete=models.CASCADE)
     content_type = models.ForeignKey(ContentType , on_delete=models.CASCADE)
     object_id = models.PositiveIntegerField()
-    contnet_object = GenericForeignKey()
+    content_object = GenericForeignKey()
